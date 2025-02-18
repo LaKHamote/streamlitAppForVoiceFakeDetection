1ebdba0c2fec5305707518efd62bd9e485d1737d
Lucas Corrêa <lucascboa@gmail.com>
2025-02-18 13:59:08 +0000

    ✨tmp dir to each user to save model

--- components/model.py
+++ components/model.py
@@ -1,6 +1,7 @@
 from fastai.vision.all import *
 import streamlit as st
 import os
+from context.userContext import getUserContext
 
 class VoiceFakeDetection:
     def __init__(self) -> None:
@@ -17,21 +18,19 @@ class VoiceFakeDetection:
             'Random Crop': RandomCrop(128),
         }
 
-        #sys.stdout = StreamlitLogger()
+        # sys.stdout = StreamlitLogger()
+        getUserContext() # reload user context between sessions(refresh)
         self.save_path = f".tmp/{st.session_state.username}"
-        try:
-            os.mkdir(self.save_path)
-        except:
-            pass
+        os.makedirs(self.save_path, exist_ok=True)
+
 
     def train_model(self, architecture_name, transform_type, zipFile, num_epochs, num_batches, callbacks) -> None:
-        st.session_state.training_out = st.empty()
-        st.session_state.training_table = st.empty()
-        st.session_state.progress =  st.progress(0)
         # tmp_path = "/".join(zipFile.split('/')[:-1])
         # if zipfile.is_zipfile(zipFile.name):
         #     with zipfile.ZipFile(zipFile.name, 'r') as zip_ref:
         #         zip_ref.extractall(tmp_path)
+        st.session_state.training_out = st.empty()
+        st.session_state.progress =  st.progress(0.0)
         tmp_path="/teamspace/uploads/mini-dataset"
         if architecture_name not in self.architectures:
             st.warning("Architecture not suported.")
@@ -53,7 +52,7 @@ class VoiceFakeDetection:
 
         try:
             self.model = vision_learner(dls, self.architectures[architecture_name], metrics=F1Score(average='macro'), path=self.save_path)
-            self.model.fine_tune(num_epochs, cbs=[eval(callbacks), LogCallback])
+            self.model.fine_tune(num_epochs, cbs=[eval(callbacks), LogCallback, CSVLogger])
 
             weights_path = f"{architecture_name}_{transform_type}.pkl"
             self.model.export(weights_path)
@@ -66,37 +65,54 @@ class VoiceFakeDetection:
             st.error(f"Error in training: {str(e)}")
 
     def __plot_loss(self):
-        # Exemplo de uso do layout com colunas
+        st.code(self.model.recorder.metric_names)
+        st.code(self.model.recorder.values)
+
         col1, col2 = st.columns(2)
 
         with col1:
             st.subheader("📉 Training & Validation Loss")
             fig, ax = plt.subplots(figsize=(3, 3))
             self.model.recorder.plot_loss(ax=ax)
-            st.pyplot(fig, use_container_width=False)  # Use container width for better layout
+            st.pyplot(fig, use_container_width=False)
 
         with col2:
             st.subheader("🔢 Confusion Matrix")
             interp = ClassificationInterpretation.from_learner(self.model)
             fig, ax = plt.subplots(figsize=(4, 4))
             interp.plot_confusion_matrix()
-            st.pyplot(fig, use_container_width=False)  # Use container width for better layout
+            st.pyplot(fig, use_container_width=False)
+
+        # st.code(self.model.log)
+
+        
+        
+
 
 
 class LogCallback(Callback):
+    # def before_fit(self):
+    #     st.session_state.training_out = st.empty()
+    #     st.session_state.progress =  st.progress(0.0)
+
     def after_batch(self):
         if self.training:
             batch = self.iter
             loss = self.loss.item()
             st.session_state.training_out.code(f"Batch {batch}: Loss {loss:.4f}")
-            st.session_state.progress.progress(batch/self.n_iter)
+            st.session_state.progress.progress(batch/ self.n_iter)
+
 
     def after_epoch(self):
         st.session_state.progress.progress(1.0)
-        st.session_state.training_out.code(f"Epoch {self.epoch+1} complete!")
+        st.session_state.training_out.code(f"Epoch {self.epoch} complete!")
+    
+    # def before_epoch(self):
+    #     fig, ax = plt.subplots(figsize=(3, 3))
+    #     self.recorder.plot_loss(ax=ax)
+    #     st.session_state.training_table.pyplot(fig, use_container_width=False)  # Use container width for better layout
 
-class StreamlitLogger(io.StringIO):
-    def write(self, message):
-        # st.session_state.training_out.text(message.strip())
-        # st.write(message.strip())
-        pass
+# class StreamlitLogger(io.StringIO):
+#     def write(self, message):
+#         # st.session_state.training_out.text(message.strip())
+#         st.code(message.strip())
